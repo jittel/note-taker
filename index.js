846a87a64d6ba3968f3b26841df7d209cf1591d4
jittel <nathan.c.ginn@gmail.com>
2022-04-28 15:47:28 -0700

    copied work over from UW... folder

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const database = require('./db/db.json')
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/assets/js/notes.html'))
+});
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        res.json(JSON.parse(data));
+    })
+});
+
+app.post('/api/notes', (req, res) => {
+
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a note`);
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text
+        }
+        //  read the db.json file and return all saved notes as JSON
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+
+            if (err) {
+                console.log(err)
+            } else {
+                const parsedNotes = JSON.parse(data);
+
+                parsedNotes.push(newNote);
+
+                fs.writeFile(
+                    './db/db.json',
+                    JSON.stringify(parsedNotes, null, 4),
+                    (writeErr) =>
+                        writeErr
+                            ? console.error(writeErr)
+                            : console.info('Successfully updated notes!')
+                );
+            }
+        })
+    } else {
+        res.status(500).json('Error in posting review');
+    }
+})
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
